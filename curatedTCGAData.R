9c0ee88f6d847ed45c8020ddd2fdfc330cb610a2
ZiyuWang1121 <78810801+ZiyuWang1121@users.noreply.github.com>
2024-03-26 15:02:35 +0800

    Add files via upload

---
+++ curatedTCGAData.R
@@ -0,0 +1,65 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+#BiocManager::install("curatedTCGAData")
+#BiocManager::install("TCGAutils")
+
+library(curatedTCGAData)
+library(MultiAssayExperiment)
+library(TCGAutils)
+
+#browseVignettes("curatedTCGAData")
+
+# check here for 'diseaseCode' and 'assays' inputs
+#?curatedTCGAData
+
+#curatedTCGAData("BRCA", version = "2.0.1")
+
+# "Methylation_methyl450", "CNVSNP", "miRNASeqGene"
+brca <- suppressMessages(curatedTCGAData("BRCA", c("RNASeq2GeneNorm"), version = "2.0.1", dry.run = FALSE))
+
+# Filter primary tumor cases (code 01)
+tumor <- TCGAprimaryTumors(brca)
+
+# Check for replicates
+anyReplicated(tumor)
+#   BRCA_miRNASeqGene-20160128 BRCA_RNASeq2GeneNorm-20160128 
+#   FALSE                         FALSE 
+
+cd = colData(brca)[which(!is.na(brca$OS.Time)),]
+library(survival)
+osurv = Surv(cd$OS.Time/365.25, cd$OS.event)
+# Plot overall survival curve
+#plot(survfit(osurv~1), main = "Overall BRCA Survival", xlab = "Years")
+
+# Some cancer datasets contain associated subtype info
+# within the clinical datasets
+head(getSubtypeMap(brca))
+
+summary(brca)
+
+# common clinical variables
+head(getClinicalNames("BRCA"))
+
+colData(brca)[, getClinicalNames("BRCA")][1:5, 1:5]
+
+sampleTables(brca)
+
+# code used to interpret the above information
+data(sampleTypes, package = "TCGAutils")
+sampleTypes
+
+#primaryTumors <- TCGAprimaryTumors(brca)
+
+#sampleTables(primaryTumors)
+
+# Set the working directory to disk D:
+setwd("E:/1ио©н/Capstone")
+getwd()
+
+td <- tempdir()
+tempd <- file.path(getwd(), "BRCA")
+if (!dir.exists(tempd))
+  dir.create(tempd)
+
+exportClass(tumor, dir = tempd, fmt = "csv", ext = ".csv")
